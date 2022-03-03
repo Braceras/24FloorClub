fb5fa39e5c5cbfaea195af3fbebab1875aa53382
Benja Braceras <benjabracerasm@gmail.com>
2022-03-03 11:21:57 -0300

    commit 3

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header class="header__container">
+        <a href="#"><img class="header__logo" src="" alt="24FloorClub"/></a>
+        <nav>
+            <a href="#">¿Que hacemos?</a>
+            <a href="#">Desarrolladores</a>
+            <a href="#">Contacto</a>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
