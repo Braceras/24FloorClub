82912ce757561d290c282ac5948b9e9e3c81f76c
Benja Braceras <benjabracerasm@gmail.com>
2022-03-04 13:23:26 -0300

    commit 9

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Header from './Components/Header'
-import Main from './Components/Main'
+import Main from './Components/ItemListComponent'
 import Footer from './Components/Footer'
 
 
