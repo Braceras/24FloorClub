189d343ca05b8ec29f51cd294ebea6fd2f9962e9
Benja Braceras <benjabracerasm@gmail.com>
2022-03-11 01:47:35 -0300

    commit 17

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,53 +1,20 @@
 import React, { useState } from 'react'
 import Contador from './Contador'
 import TitleLogo from './TitleLogo'
-import { useEffect } from 'react'
+import Item from './Item'
 
-let productosIiciales = [
-  {
-    id: 1,
-    nombre: 'Producto 1',
-    precio: 100 
-  },
-  {
-    id: 2,
-    nombre: 'Producto 2',
-    precio: 200 
-  },
-  {
-    id: 3,
-    nombre: 'Producto 3',
-    precio: 300 
-  },
-]
 
 const Main = () => {
 
-  const [loading, setLoading] = useState(true)
-  const [productos, setProductos] = useState([])
-
-  useEffect(() => {
-    console.log('Ejecutando useEffect')
-      
-    setTimeout(() => {
-      setProductos(productosIiciales) 
-    }, 5000)
-  },[]) 
-
-  console.log(productos)
+  
 
   return (
     <div>
 
       <TitleLogo/>
       <h2>Bienvenidos!</h2>
-      <button onClick={() => setLoading(!loading)} >toggle</button>
-      <ul>
-        {productos.map((producto) => {
-            return <li key={producto.id}>{producto.nombre}</li>
-        })}
-        
-      </ul>
+      <Item/>
+      
       <Contador/>
 
     </div>
